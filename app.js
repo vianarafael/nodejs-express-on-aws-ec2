6b5cc912bd38afa6c7e108652d1810c685ac24b9
vianarafael <vianarafaelds@gmail.com>
2021-06-09 15:54:49 +0900

    v3

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const socket = require("socket.io");
+const cors = require("cors");
+
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send(
-    "<h1>Express Demo App</h1> <h4>Message: Very Successful, Rafa?</h4> <p>Version 42.0</p>"
+    "<h1>Express Demo App</h1> <h4>Message: Is this working?</h4> <p>Version 42.0</p>"
   );
 });
 
@@ -23,4 +26,37 @@ app.get("/products", (req, res) => {
 
 const server = app.listen(port, () => {
   console.log(`Demo app is up and listening to port: ${port}`);
+});
+
+app.use(cors());
+
+// socket setup
+const io = socket(server, {
+  cors: {
+    origin: "https://multi-chess.s3.us-east-2.amazonaws.com/index.html",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true,
+  },
+});
+
+let numClients = 1;
+
+let gameRoom = Math.random();
+
+io.on("connection", (socket) => {
+  console.log("made socket connection", socket.id);
+  numClients++;
+
+  if (numClients > 2) {
+    numClients = 1;
+    gameRoom = Math.random();
+  }
+
+  socket.join(gameRoom);
+  io.emit("stats", { numClients, gameRoom });
+
+  socket.on("move", (data) => {
+    if (data.gameRoom) io.to(data.gameRoom).emit("move", data);
+  });
 });
